13e202f8c1937d747eec6599759435e32fe823ea
kishoresurendra <kishoresurendrav@gmail.com>
2022-08-29 11:30:04 +0200

    First version

---
+++ src/main/com/marcusbiel/javacourse/lesson10/MainClass.java
@@ -0,0 +1,14 @@
+package com.marcusbiel.javacourse.lesson10;
+
+public class MainClass {
+
+    public static void main(String[] args) {
+        LoggingLevel state = LoggingLevel.COMPLETED;
+        state = LoggingLevel.PROCESSING;
+        for(LoggingLevel lev:LoggingLevel.values()){
+            System.out.println(lev);
+        }
+        System.out.println(LoggingLevel.valueOf("COMPLETED"));
+    }
+
+}
